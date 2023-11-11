a9befc3681903ab2472a370ca55b747490d8c33e
Bekurt <attilio.dinunno@gmail.com>
2023-11-11 20:51:04 +0100

    Project setup

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	tailwindFunctions: ['tw'],
+	plugins: ['prettier-plugin-tailwindcss'],
+	trailingComma: 'es5',
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	bracketSameLine: true,
+	jsxSingleQuote: true,
+	printWidth: 120,
+}
