04c8cb0224a713adc7cec1f409c47f354fc4a5a3
Bekurt <attilio.dinunno@gmail.com>
2023-11-12 10:53:20 +0100

    Project Setup

---
+++ app/redux/provider.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react'
+import { store } from './store'
+import { Provider } from 'react-redux'
+
+export function ClientProvider({ children }: { children: ReactNode }) {
+	return <Provider store={store}>{children}</Provider>
+}
